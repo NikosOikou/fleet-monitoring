76a1a23dd41a4d4afb88b89ed34c16a3446801d6
Your Name <nicolaosoikou@gmail.com>
2019-07-17 15:26:30 +0200

    Added functionality

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
 import requests
-from geojson import Point, Feature
+from geojson import Feature, Point
 
 # Mapbox driving direction API call
 ROUTE_URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/{0}.json?access_token={1}&overview=full&geometries=geojson'
@@ -8,19 +8,18 @@ ROUTE_URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/{0}.json?access
 def get_stop_locations(route):
     stop_locations = []
     for route_index, location in enumerate(route):
-        if not location['is_stop_location']:
-            continue
         point = Point([location['long'], location['lat']])
         properties = {
-            'title': location['name'],
+            'title': location['title'],
             'icon': 'campsite',
             'marker-color': '#3bb2d0',
-            'marker-symbol': len(stop_locations) + 1,
+            'marker-symbol': route_index + 1,
             'route_index': route_index
         }
         stop_location = Feature(geometry=point, properties=properties)
         stop_locations.append(stop_location)
     return stop_locations
+
 
 def get_route_data(mapbox_access_key, route):
     lat_longs = ';'.join([
@@ -30,4 +29,4 @@ def get_route_data(mapbox_access_key, route):
     result = requests.get(route_url)
     data = result.json()
     geometry = data['routes'][0]['geometry']
-    return Feature(geometry = geometry, properties = {})
+    return Feature(geometry=geometry, properties={})
--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-import json
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
-from helper import get_stop_locations, get_route_data
+from helper import get_route_data, get_stop_locations
+from process import (avg_speed_by_hour, max_wind_speed_by_hour,
+                     min_wind_speed_by_hour, num_ships, route_weather_data)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -11,30 +12,46 @@ app.config.from_object(__name__)
 MAPBOX_ACCESS_KEY = os.environ.get('MAPBOX_ACCESS_KEY')
 DEBUG = os.environ.get('DEBUG')
 
-ROUTE = [{'lat': 51.87622233333333, 'long': 5.7971985,'name': '{Skatokairos olakala}', 'is_stop_location': True},
- {'lat': 51.8982955, 'long': 5.646801166666666,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.90278333333333, 'long': 5.542553,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.90100066666667, 'long': 5.541878333333333,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.8544775, 'long': 5.416200666666667,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.816728166666664, 'long': 5.175681833333333,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.824829, 'long': 4.969789166666667,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.797950166666666, 'long': 4.827218,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.695032499999996, 'long': 4.5069165,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.69839916666667, 'long': 4.424386166666666,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.68235283333333, 'long': 4.395956666666667,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.6836035, 'long': 4.393018666666666,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.683595333333336, 'long': 4.39302,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.683601833333334, 'long': 4.393012,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.683608166666666, 'long': 4.393020833333333,'name': 'Skatokairos', 'is_stop_location': True},
- {'lat': 51.683597, 'long': 4.393012833333334,'name': 'Skatokairos', 'is_stop_location': True}]
+ROUTE = [
+    {
+        'lat': d['lat_msg'],
+        'long': d['lon_msg'],
+        'is_stop_location': True,
+        'title':  ','.join([
+            """Station: {0},
+               Date: {1},
+               Time: {2},
+               Weather: {3},
+               Temperature: {4} \u00b0C,
+               Wind: {5:.2f} km/h
+            """.format(
+                d['city_name'],
+                d['date_msg'],
+                d['time_msg'],
+                d['description'],
+                d['temp'],
+                d['wind_spd']
+            )
+        ])
+    }
+    for d in route_weather_data
+]
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/data')
-def data():
-    return render_template('data.html')
+
+@app.route('/metrics')
+def metrics():
+    return {
+        'num_ships': num_ships,
+        'avg_speed_by_hour': avg_speed_by_hour,
+        'min_wind_speed_by_hour': min_wind_speed_by_hour,
+        'max_wind_speed_by_hour': max_wind_speed_by_hour,
+    }
+
 
 @app.route('/route')
 def route():
@@ -44,11 +61,12 @@ def route():
     route_data = get_route_data(MAPBOX_ACCESS_KEY, ROUTE)
     stop_locations = get_stop_locations(ROUTE)
     return render_template(
-        'route.html', 
+        'route.html',
         ACCESS_KEY=MAPBOX_ACCESS_KEY,
         route_data=route_data,
-        stop_locations = stop_locations
+        stop_locations=stop_locations
     )
+
 
 if __name__ == '__main__':
     DEBUG = int(os.environ.get('DEBUG', 0)) == 1
